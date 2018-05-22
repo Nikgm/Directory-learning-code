df5aaeb3c3fda362e547ad6c2cefbcdaedf205b7
Nikgm <39501813+Nikgm@users.noreply.github.com>
2018-05-21 23:08:05 -0400

    Add files via upload

---
+++ Directory.py
@@ -0,0 +1,25 @@
+
+title = """Welcome to the Programming Language Directory:
+1. Python
+2. Java
+3. C++
+4. Ruby
+5. HTML
+6. Other""" ##Title statement
+
+print title ##Print Title statement
+
+input1 = raw_input("What programming language would you like to explore? ")
+
+if input1 == "1":
+    py_directory = """PYTHON:
+    1. functions"""
+
+    print py_directory
+    input1_1 = raw_input("What do you want to learn about Python? ")
+    if input1_1 == "1":
+        print /n """functions have 3 components to them. A header which defines the function and includes the def() keyword. For example:
+def python() #this defines the function
+print "this is the python language" #this will print the text according to the functions"""
+
+exit = raw_input("Press anything to close")
